3d873ef361e4bd92ab38ae9b341bbacb3f3fadb4
Мангир Александр <sasha982025@gmail.com>
2018-12-18 23:40:01 +0200

    xor

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Layer, Network } from "synaptic";
+
+const inputLayer = new Layer(2);
+const hiddenLayer = new Layer(3);
+const outputLayer = new Layer(1);
+
+inputLayer.project(hiddenLayer);
+hiddenLayer.project(outputLayer);
+
+const myNetwork = new Network({
+  input: inputLayer,
+  hidden: [hiddenLayer],
+  output: outputLayer
+});
 
 class App extends Component {
+  state = {
+    first: undefined,
+    sec: undefined,
+    thr: undefined,
+    fo: undefined
+  };
+  f = () => {
+    const learningRate = 0.1;
+    for (let i = 0; i < 20000; i++) {
+      // 0,0 => 0
+      myNetwork.activate([0, 0]);
+      myNetwork.propagate(learningRate, [0]);
+
+      // 0,1 => 1
+      myNetwork.activate([0, 1]);
+      myNetwork.propagate(learningRate, [1]);
+
+      // 1,0 => 1
+      myNetwork.activate([1, 0]);
+      myNetwork.propagate(learningRate, [1]);
+
+      // 1,1 => 0
+      myNetwork.activate([1, 1]);
+      myNetwork.propagate(learningRate, [0]);
+    }
+    this.setState({
+      first: myNetwork.activate([0, 0]),
+      sec: myNetwork.activate([0, 1]),
+      thr: myNetwork.activate([1, 0]),
+      fo: myNetwork.activate([1, 1])
+    });
+  };
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="text">
+        <br />
+        <button onClick={this.f}>start</button>
+        <br />
+        {this.state.first} <br />
+        {this.state.sec} <br />
+        {this.state.thr} <br />
+        {this.state.fo}
       </div>
     );
   }
